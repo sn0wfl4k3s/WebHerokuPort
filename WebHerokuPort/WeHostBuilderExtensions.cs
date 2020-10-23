8223c259aa7b3c2e16ba5ff52aefa955528646f9
Flakes <ed.afram@gmail.com>
2020-10-22 23:10:04 -0300

    library criada

---
+++ WebHerokuPort/WeHostBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace WebHerokuPort
+{
+    public static class WeHostBuilderExtensions
+    {
+        /// <summary>
+        /// Try use the port setting in enviroment heroku ($PORT) if exist.
+        /// </summary>
+        /// <param name="builder"></param>
+        /// <returns></returns>
+        public static IWebHostBuilder UseHerokuPort(this IWebHostBuilder builder)
+        {
+            string port = Environment.GetEnvironmentVariable("PORT");
+
+            if (string.IsNullOrEmpty(port))
+            {
+                return builder;
+            }
+
+            return builder.UseUrls($"http://+:{port}");
+        }
+    }
+}
